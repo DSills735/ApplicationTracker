1de6b17bc3ff4e4b7f4de13246e4974025dab4ba
DSills735 <David.sills100@gmail.com>
2026-05-12 08:42:59 -0500

    Add project files.

---
+++ ApplicationTracker.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+
+
+class Application:
+      def __init__(self, company, date_applied, status_if_known, position, notes):
+           self.company = company
+           self.date_applied = date_applied
+           self.status_if_known = status_if_known
+           self.position = position
+           self.notes = notes
+
+conn = sqlite3.connect('applications.db')
+c = conn.cursor()
+
+c.execute("CREATE TABLE IF NOT EXISTS applications (company TEXT, date_applied TEXT, status_if_known TEXT, position TEXT, notes TEXT)")")
+conn.commit()
+conn.close()
+
